24e725d5898a2cbd9be52021c6ad8af447788161
Mostafa Nasser <mostafanasserm98@gmail.com>
2023-11-25 15:38:16 +0200

    Duck intalize mehtod in class finished and implemnting random function

---
+++ scripts/GameManger.js
@@ -0,0 +1,8 @@
+let duckObjet= new Duck(900,300); // you should call the fuction with respect to the height and the width of the page
+
+duckObjet = duckObjet.intalizeDuck();
+
+
+let containerDiv= document.querySelector('.container');
+
+containerDiv.appendChild(duckObjet);
